a1f80575f4d8928bda4368557bc65447c26841b9
Kelvin.GKB <43901576+KelvinGKB@users.noreply.github.com>
2020-07-25 13:38:53 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# snake_ladder_game
+project got snake and ladder game
---
+++ src/snake_ladder_game/Snake_ladder_game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake_ladder_game;
+
+/**
+ *
+ * @author kengboongoh
+ */
+public class Snake_ladder_game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

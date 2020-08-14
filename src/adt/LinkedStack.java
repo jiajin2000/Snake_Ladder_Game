d0c7ffdc3711d88c6b87e74e9a13a4a353cabce9
jiajin <>
2020-08-14 19:40:36 +0800

    LinkedStack

---
+++ src/adt/LinkedStack.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adt;
+
+/**
+ *
+ * @author jiajin
+ */
+public class LinkedStack<T> implements LinkedStackInterface<T>{
+    
+    private class Node<T>{
+        T item;
+        Node<T> next;
+        private Node(T item, Node<T> next){
+            this.item = item;
+            this.next = next;
+            }
+        private Node(){
+            item = null;
+            next = null;
+        }
+    }
+    private Node<T> top;
+    private int size;
+    
+    public LinkedStack(){
+        top = null;
+        size = 0;
+        }
+    public void push(T element){
+        Node<T> newNode = new Node(element,top);
+        this.top = newNode;
+        size++;
+        }
+    public T pop(){
+        if(this.top == null){
+            System.out.print("Stack is empty");
+            }
+        T returnElement = this.top.item;
+        this.top = top.next;
+        size--;
+        return returnElement;
+        }
+    public T peek(){
+       if(this.top == null){
+             System.out.print("Stack is empty");
+            }
+        T returnElement = this.top.item;
+        return returnElement;
+        }
+    public int size(){
+        return this.size;
+        }
+    
+}

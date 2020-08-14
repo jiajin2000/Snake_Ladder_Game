5dc8280c19e94dc4cfd3e8a55d5381cb4b3ea6af
Kelvin1 <kengboonogoh@gmail.com>
2020-08-14 09:08:35 +0800

    SorLSquare Entity

---
+++ src/entity/SorLSquare.java
@@ -0,0 +1,60 @@
+package entity;
+
+/**
+ *
+ * @author kengboongoh
+ */
+public class SorLSquare {
+  
+  private static int nextNumber = 1;
+  private int number;
+  private String type;
+  private int startSquare;
+  private int endSquare;
+
+  public SorLSquare(String type, int startSquare, int endSquare) {
+    this.startSquare = startSquare;
+    this.endSquare = endSquare;
+    this.type=type;
+    this.number = nextNumber++;
+  }
+  
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+  
+   public int getEndSquare() {
+    return endSquare;
+  }
+
+  public void setEndSquare(int endSquare) {
+    this.endSquare = endSquare;
+  }
+  
+   public int getStartSquare() {
+    return startSquare;
+  }
+
+  public void setStartSquare(int startSquare) {
+    this.startSquare = startSquare;
+  }
+
+
+  @Override
+  public String toString() {
+    return String.format("SorLSquare No. %-5d Type : %-10s From %d to %d \n", number, type,startSquare,endSquare);
+  }
+    
+}

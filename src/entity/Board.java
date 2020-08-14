4b5406df78b2c81e4d8e9c11abfae84f65585937
jy930 <jy930@LAPTOP-0LH88962>
2020-08-14 14:29:33 +0800

    add linkedlist and board

---
+++ src/entity/Board.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author 
+ */
+public class Board {
+    
+    private String boardNo;
+
+  public Board(String boardNo) {
+    this.boardNo= boardNo;
+  }
+    public String getBoardNo() {
+        return boardNo;
+    }
+
+    public void setBoardNo(String boardNo) {
+        this.boardNo = boardNo;
+    }
+
+  @Override
+  public String toString() {
+    return String.format("Board NO : %s", boardNo);
+  }
+  
+}
